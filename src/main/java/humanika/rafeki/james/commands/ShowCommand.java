5d914a3485835700fdc15022f85a68a820f1a3cd
UnorderedSigh <116329264+UnorderedSigh@users.noreply.github.com>
2023-12-30 01:53:40 +0000

    fixes for long descriptions

--- src/main/java/humanika/rafeki/james/commands/ShowCommand.java
+++ src/main/java/humanika/rafeki/james/commands/ShowCommand.java
@@ -20,10 +20,13 @@ import me.mcofficer.esparser.DataNode;
 import discord4j.core.object.Embed;
 import discord4j.core.object.entity.Message;
 import java.util.Arrays;
+import java.util.function.BooleanSupplier;
 
 public class ShowCommand extends SlashCommand {
     protected final static int QUERY_COUNT = 14;
     protected final static int PRIMARY_COUNT = 6;
+    protected final static int MAX_CHARS_PER_FIELD = 1000;
+
     @Override
     public String getName() {
         return "show";
@@ -122,75 +125,130 @@ public class ShowCommand extends SlashCommand {
         StringBuilder builder = new StringBuilder(100);
         String before = James.getConfig().endlessSkyData;
         String after = James.getConfig().endlessSkyDataQuery;
+        AddParagraphFields fieldAdder = new AddParagraphFields(4);
         for(NodeInfo info : found) {
             List<EmbedCreateFields.Field> fields = new ArrayList<>();
 
             Optional<List<DataNode>> descriptions = info.getDescription();
-            if(descriptions.isPresent()) {
-                builder.delete(0, builder.length());
-                for(DataNode node : descriptions.get())
-                    if(node.size() > 1) {
-                        if(builder.length() > 0)
-                            builder.append('\n');
-                        builder.append(node.token(1));
-                    }
-                fields.add(EmbedCreateFields.Field.of("Description", builder.toString(), false));
-            }
+            if(descriptions.isPresent())
+                fieldAdder.add("Description", fields, descriptions.get());
 
+            fieldAdder.setMaxFields(5);
             Optional<List<DataNode>> spaceport = info.getSpaceport();
-            if(spaceport.isPresent()) {
-                builder.delete(0, builder.length());
-                for(DataNode node : spaceport.get())
-                    if(node.size() > 1) {
-                        if(builder.length() > 0)
-                            builder.append('\n');
-                        builder.append(node.token(1));
-                    }
-                fields.add(EmbedCreateFields.Field.of("Spaceport", builder.toString(), false));
-            }
+            if(spaceport.isPresent())
+                fieldAdder.add("Spaceport", fields, spaceport.get());
 
             if(fields.size() < 1)
                 fields.add(EmbedCreateFields.Field.of("Description", "*no description*", false));
 
-            Optional<String> maybeThumbnail = info.getBestThumbnail();
-            Optional<String> maybeImage = info.getBestImage();
-            String image = null;
-            String thumbnail = null;
-            if(maybeImage.isPresent())
-                image = maybeImage.get();
-            if(maybeThumbnail.isPresent())
-                thumbnail = maybeThumbnail.get();
-            if(image == thumbnail)
-                thumbnail = null;
-
-            if(image != null) {
-                maybeImage = James.getState().getImageRawUrl(image);
-                image = maybeImage.orElse(null);
-            }
-            if(thumbnail != null) {
-                maybeThumbnail = James.getState().getImageRawUrl(thumbnail);
-                thumbnail = maybeThumbnail.orElse(null);
-            }
-
-            if(image == null && thumbnail != null) {
-                image = thumbnail;
-                thumbnail = null;
-            }
-
             EmbedCreateSpec embed = EmbedCreateSpec.create().withFields(fields)
                 .withTitle(info.getBestType() + ' ' + info.getName());
-            if(image != null)
-                embed = embed.withImage(image);
-            if(thumbnail != null)
-                embed = embed.withThumbnail(thumbnail);
+
+            String[] imageAndThumbnail = getImageAndThumbnail(info);
+            if(imageAndThumbnail[0] != null)
+                embed = embed.withImage(imageAndThumbnail[0]);
+            if(imageAndThumbnail[1] != null)
+                embed = embed.withThumbnail(imageAndThumbnail[1]);
             embeds.add(embed);
         }
         Mono<Message> newReply = event.getReply().flatMap(reply -> event.editReply().withEmbeds(embeds));
         newReply.block();
     }
 
+    protected String[] getImageAndThumbnail(NodeInfo info) {
+        Optional<String> maybeThumbnail = info.getBestThumbnail();
+        Optional<String> maybeImage = info.getBestImage();
+        String image = null;
+        String thumbnail = null;
+        if(maybeImage.isPresent())
+            image = maybeImage.get();
+        if(maybeThumbnail.isPresent())
+            thumbnail = maybeThumbnail.get();
+        if(image == thumbnail)
+            thumbnail = null;
+
+        if(image != null) {
+            maybeImage = James.getState().getImageRawUrl(image);
+            image = maybeImage.orElse(null);
+        }
+        if(thumbnail != null) {
+            maybeThumbnail = James.getState().getImageRawUrl(thumbnail);
+            thumbnail = maybeThumbnail.orElse(null);
+        }
+
+        if(image == null && thumbnail != null) {
+            image = thumbnail;
+            thumbnail = null;
+        }
+
+        String[] result = { image, thumbnail };
+        return result;
+    }
+
     protected Optional<List<NodeInfo>> getMatches(String query) {
         return James.getState().fuzzyMatchNodeNames(query, QUERY_COUNT, info -> !info.isShipVariant() && (info.hasDescription() || info.hasSpaceport() || info.hasImage()) );
+    }
+
+    protected class AddParagraphFields {
+        private int fieldsSent = 0;
+        private int paragraphsInField = 0;
+        private StringBuilder builder = new StringBuilder(100);
+        private int maxFields;
+
+        public AddParagraphFields(int maxFields) {
+            this.maxFields = maxFields;
+        }
+
+        public void setMaxFields(int maxFields) {
+            this.maxFields = maxFields;
+        }
+
+        public int getMaxFields() {
+            return maxFields;
+        }
+
+        public void add(String title, List<EmbedCreateFields.Field> fields, List<DataNode> paragraphs) {
+            paragraphsInField = 0;
+            builder.delete(0, builder.length());
+
+            for(DataNode node : paragraphs) {
+                if(node.size() < 1)
+                    continue;
+                
+                String paragraph = node.token(1);
+                int maxFieldSize = MAX_CHARS_PER_FIELD;
+                if(fieldsSent == 0)
+                    maxFieldSize -= title.length() + 1;
+                
+                if(builder.length() + 1 + paragraph.length() > maxFieldSize) {
+                    if(!addField(title, fields))
+                        break;
+                    if(paragraph.length() > maxFieldSize) {
+                        builder.append(paragraph.substring(0, maxFieldSize - 3)).append("...");
+                        if(!addField(title, fields))
+                            break;
+                        continue;
+                    }
+                }
+                if(paragraphsInField > 0)
+                    builder.append('\n');
+                builder.append(paragraph);
+                paragraphsInField++;
+                if(builder.length() > 0 && fieldsSent < maxFields)
+                    addField(title, fields);
+            }
+        }
+
+        private boolean addField(String title, List<EmbedCreateFields.Field> fields) {
+            if(fieldsSent == 0)
+                fields.add(EmbedCreateFields.Field.of(title, builder.toString(), false));
+            else
+                fields.add(EmbedCreateFields.Field.of("", builder.toString(), false));
+            fieldsSent++;
+            paragraphsInField = 0;
+            builder.delete(0, builder.length());
+            return fieldsSent < maxFields;
+        }
     }
 
     protected void getResponse(List<NodeInfo> matches, List<String> listItem, List<String> buttonText, List<String> buttonId, boolean ephemeral) {
